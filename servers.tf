ca17dd7c3a956debc8b0bc9f03722ad79c6ad210
belloblj2 <belloblj2@gmail.com>
2023-09-19 06:06:53 -0600

    terraform automated server creation

--- servers.tf
+++ servers.tf
@@ -1,3 +1,13 @@
+data "aws_ami" "centos" {
+  owners           = ["755011271549"]
+  most_recent      = true
+  name_regex       = "Centod-8-DevOps-Practice"
+}
+
+output "ami" {
+  value = data.aws_ami.centos.image_id
+}
+
 resource "aws_instance" "frontend" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
